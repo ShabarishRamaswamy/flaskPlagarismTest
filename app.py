deefd624063c95d1fc18afba991942997c0ecdc5
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2021-02-27 21:54:50 +0530

    User Input in JSON and output in JSON

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
 import json
 import os
 import time
+# import plagiarism
+from waitress import serve
 
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+app.debug = True
 
 # Home Route
 @app.route('/')
-def trending():
-   return render_template('home.html', info="")
+def homeRoute():
+   return "Hello World"
 
 # Values Input Route
-@app.route('/input/<id>', methods=['POST'])
+@app.route("/input", methods=['POST'])
 def inputValues():
-   pass
+       userInput = request.get_json(silent=True)
+       print(userInput)
+       return userInput
 
 
-@app.route('/output/<id>', methods=['GET'])
+@app.route("/output", methods=['GET'])
 def outputValues():
-   pass
+   userOutput = 0
+   if(userOutput):
+      return "Hello"
+   return "Hello"
 
 
 if __name__ == "__main__":
-    app.run()
+      #  port = process.env.PORT || 8080
+       serve(app, host="0.0.0.0", port = 8080)
+       
